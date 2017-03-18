7b37251cdc38b52d899709da19e3dd52598f22c1
test <mor1l@yandex.ua>
2017-03-19 01:02:48 +0200

    add login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+    <section>
+        <div class="container">
+            <div class="row">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                <div class="col-sm-4 col-sm-offset-4 padding-right">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                    <?php if (count($errors) > 0): ?>
+                    <ul>
+                        <?php foreach ($errors->all() as $error): ?>
+                        <li> - <?php echo $error; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php endif; ?>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="signup-form"><!--sign up form-->
+                        <h2>Вход на сайт</h2>
+                        <form action="{{ url('/login') }}" method="post">
+                            {{ csrf_field() }}
+                            <input id="email" type="email" name="email" placeholder="E-mail" value="{{ old('email') }}" required autofocus/>
+                            <input id="password" type="password" name="password" placeholder="Пароль" required/>
+                            <input type="submit" name="submit" class="btn btn-default" value="Вход" />
+                        </form>
+                    </div><!--/sign up form-->
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                    <br/>
+                    <br/>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
