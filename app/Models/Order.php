3fbefce1785f8339f653f6bce76e484ca7ddf23f
test <mor1l@yandex.ua>
2017-03-22 20:58:54 +0200

    add new models

---
+++ app/Models/Order.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'product_order';
+    public $timestamps = false;
+}
