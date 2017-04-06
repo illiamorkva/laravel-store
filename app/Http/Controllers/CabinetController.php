2e9987fa28a3cc91c5574341d181ee549aff4e3e
test <mor1l@yandex.ua>
2017-04-06 20:48:58 +0300

    add cabinet controller

--- app/Http/Controllers/CabinetController.php
+++ app/Http/Controllers/CabinetController.php
@@ -10,4 +10,45 @@ class CabinetController extends Controller
     {
         $this->middleware('auth');
     }
+
+    /**
+     * Action for the page "User Cabinet"
+     */
+    public function actionIndex(Request $request)
+    {
+        //Get current user
+        $user = $request->user();
+
+        return view('cabinet.index', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Action for the page "The edit user data"
+     */
+    public function actionEdit(Request $request)
+    {
+        //Get current user
+        $user = $request->user();
+
+        $name = $user->name;
+
+        $result = false;
+
+        if ($request->isMethod('post')) {
+
+            $this->validate($request, [
+                'name' => 'required|min:2',
+            ]);
+
+            $user->name = $request->input('name');
+
+            $result = $user->save();
+        }
+
+        return view('cabinet.edit', [
+            'name' => $name, 'result' => $result
+        ]);
+    }
 }
