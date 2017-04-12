e2137957a15fb516fdbe12c60fef941e728fea1e
test <mor1l@yandex.ua>
2017-04-12 16:36:50 +0300

    add admin products module

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CategoryRepository;
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
 {
-    //
+    /**
+     * Instance CategoryRepository.
+     *
+     * @var CategoryRepository
+     */
+    protected $categoryRepository;
+
+    /**
+     * Instance ProductRepository.
+     *
+     * @var ProductRepository
+     */
+    protected $productRepository;
+
+
+    public function __construct(CategoryRepository $categoryRepository, ProductRepository $productRepository)
+    {
+        $this->middleware(['auth','role:admin']);
+
+        $this->categoryRepository = $categoryRepository;
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * Action for page "Manage products"
+     */
+    public function actionIndex()
+    {
+        //Get information about products
+        $productsList = $this->productRepository->getProductsList();
+
+        return view('admin_product.index', [
+             'productsList' => $productsList
+        ]);
+    }
 }
