ac149011335fd637651db16f2e9bfa96d3026771
test <mor1l@yandex.ua>
2017-04-19 18:46:15 +0300

    add update function admin product

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -90,4 +90,49 @@ class AdminProductController extends Controller
             'categoriesList' => $categoriesList
         ]);
     }
+
+    /**
+     * Action for page "Update product"
+     */
+    public function actionUpdate(Request $request, $id)
+    {
+        //Get the list of categories for dropdown
+        $categoriesList = $this->categoryRepository->getCategoriesListAdmin();
+
+        // Get data about current product
+        $product = $this->productRepository->getProductById($id);
+
+        if ($request->isMethod('post')) {
+
+            $this->validate($request, [
+                'name' => 'required|min:2',
+                'code' => 'required|min:2|numeric',
+                'price' => 'required|min:2|numeric',
+            ]);
+
+            $product->name = $request->input('name');
+            $product->code = $request->input('code');
+            $product->price = $request->input('price');
+            $product->category_id = $request->input('category_id');
+            $product->brand = $request->input('brand');
+            $product->availability = $request->input('availability');
+            $product->description = $request->input('description');
+            $product->is_new = $request->input('is_new');
+            $product->is_recommended = $request->input('is_recommended');
+            $product->status = $request->input('status');
+
+            if($product->save()){
+                if (is_uploaded_file($_FILES["image"]["tmp_name"])) {
+                    move_uploaded_file($_FILES["image"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . "/upload/images/products/{$id}.jpg");
+                }
+            }
+
+            return redirect("/admin/product");
+        }
+        return view('admin_product.update', [
+            'categoriesList' => $categoriesList,
+            'product' => $product,
+            'id' => $id
+        ]);
+    }
 }
