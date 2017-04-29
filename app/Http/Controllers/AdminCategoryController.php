700ab37fd9f4bfd27c265b851822ee62ed1a682d
test <mor1l@yandex.ua>
2017-04-29 12:10:56 +0300

    add admin category create function

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Repositories\CategoryRepository;
 use Illuminate\Http\Request;
 
@@ -33,5 +34,28 @@ class AdminCategoryController extends Controller
         return view('admin_category.index', [
             'categoriesList' => $categoriesList
         ]);
+    }
+
+    /**
+     * Action for page "Add category"
+     */
+    public function actionCreate(Request $request)
+    {
+        if ($request->isMethod('post')) {
+
+            $this->validate($request, [
+                'name' => 'required',
+                'sort_order' => 'required|min:1|numeric',
+            ]);
+
+            $category = new Category();
+            $category->name = $request->input('name');
+            $category->sort_order = $request->input('sort_order');
+            $category->status = $request->input('status');
+            $category->save();
+
+            return redirect("admin/category");
+        }
+        return view('admin_category.create');
     }
 }
