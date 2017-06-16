3a4ba6615be45284483e78b147cf4ed93ab76382
test <mor1l@yandex.ua>
2017-06-16 18:26:22 +0300

    add search input

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,7 +104,7 @@
         <div class="header-bottom"><!--header-bottom-->
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-12">
+                    <div class="col-sm-9">
                         <div class="navbar-header">
                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                                 <span class="sr-only">Toggle navigation</span>
@@ -125,6 +125,11 @@
                                 <li><a href="/about/">О магазине</a></li>
                                 <li><a href="/contacts/">Контакты</a></li>
                             </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="search_box pull-right">
+                            <input type="text" placeholder="Search"/>
                         </div>
                     </div>
                 </div>
